f3d26a79cfc617d3eb288aa8fe87d0a5fca5b239
Caio Povill Galvão <caio_bgalvao@hotmail.com>
2025-12-05 23:04:55 -0300

    feat: Add initial main C program file

---
+++ main.c
@@ -0,0 +1,9 @@
+/* =========================================================================
+ * Arquivo: main.c
+ * Objetivo: Menu principal e fluxo de execução do programa.
+ *
+ * Autoria:
+ *   Nome:      [NOME DO ESTUDANTE AQUI]
+ *   Matrícula: [MATRICULA AQUI]
+ *   Data:      2025-12-06
+ * ========================================================================= */
