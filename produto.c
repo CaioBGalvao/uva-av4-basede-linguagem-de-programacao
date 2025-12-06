fdf9fbee13de0b2fda3992b4c0b93838f0de7240
Caio Povill Galvão <caio_bgalvao@hotmail.com>
2025-12-05 23:06:02 -0300

    feat: add initial product catalog module file `produto.c`

---
+++ produto.c
@@ -0,0 +1,9 @@
+/* =========================================================================
+ * Arquivo: produtos.c
+ * Objetivo: Implementação do módulo de catálogo de produtos.
+ *
+ * Autoria:
+ *   Nome:      [NOME DO ESTUDANTE AQUI]
+ *   Matrícula: [MATRICULA AQUI]
+ *   Data:      2025-12-06
+ * ========================================================================= */
