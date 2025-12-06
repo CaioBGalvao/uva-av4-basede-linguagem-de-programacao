005115a1f0bb97c0b622b5c3cae19f3cfa9b2b6f
Caio Povill Galvão <caio_bgalvao@hotmail.com>
2025-12-05 23:03:51 -0300

    feat: introduce freight calculation module boilerplate

---
+++ frete.c
@@ -0,0 +1,9 @@
+/* =========================================================================
+ * Arquivo: frete.c
+ * Objetivo: Implementação do módulo de cálculo de frete.
+ *
+ * Autoria:
+ *   Nome:      [NOME DO ESTUDANTE AQUI]
+ *   Matrícula: [MATRICULA AQUI]
+ *   Data:      2025-12-06
+ * ========================================================================= */
