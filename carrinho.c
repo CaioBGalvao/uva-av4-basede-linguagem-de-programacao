8578b2d316e23f5fc2c6920b7fa4de191e6d796b
Caio Povill Galvão <caio_bgalvao@hotmail.com>
2025-12-05 23:03:04 -0300

    feat: implement shopping cart logic.

---
+++ carrinho.c
@@ -0,0 +1,9 @@
+/* =========================================================================
+ * Arquivo: carrinho.c
+ * Objetivo: Implementação da lógica do carrinho de compras.
+ *
+ * Autoria:
+ *   Nome:      [NOME DO ESTUDANTE AQUI]
+ *   Matrícula: [MATRICULA AQUI]
+ *   Data:      2025-12-06
+ * ========================================================================= */
