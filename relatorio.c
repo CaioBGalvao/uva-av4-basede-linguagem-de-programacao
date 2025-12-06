1fefad4f123105576b11dd893eeac9bc7fa5f7ac
Caio Povill Galvão <caio_bgalvao@hotmail.com>
2025-12-05 23:07:09 -0300

    feat: Add C source file for report functionality.

---
+++ relatorio.c
@@ -0,0 +1,9 @@
+/* =========================================================================
+ * Arquivo: relatorio.c
+ * Objetivo: Implementação da geração de Nota Fiscal e arquivos.
+ *
+ * Autoria:
+ *   Nome:      [NOME DO ESTUDANTE AQUI]
+ *   Matrícula: [MATRICULA AQUI]
+ *   Data:      2025-12-06
+ * ========================================================================= */
