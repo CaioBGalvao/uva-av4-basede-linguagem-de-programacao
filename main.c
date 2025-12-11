0f46a0754c85d7df14a12ae69b2686e049c7a110
an4beatrizsilva <annabiah483@gmail.com>
2025-12-10 21:20:24 -0300

    chamei as funções e concertei o o final

--- main.c
+++ main.c
@@ -17,16 +17,20 @@ int main()
     printf("     Bem-vindo ao Flaviozon!  \n");
     printf("=====================================\n");
 
-    while (opcao != 3)
+    while (opcao != 5) // funções
     {
         printf("\nMenu Principal:\n");
-        printf("1 - Comprar\n");
+        printf("1 - Comprar Produto\n");
         printf("2 - Ver Carrinho\n");
-        printf("3 - Finalizar Pedido\n");
-        NewFunction();
+        printf("3 - Calcular Frete\n");
+        printf("4 - Gerar Relatorio\n");
+        printf("5 - Finalizar Pedido\n");
+
+        NewFunction(); // "Escolha uma opção:"
+
         if (scanf("%d", &opcao) != 1)
         {
-            // limpar entrada inválida
+        
             int c;
             while ((c = getchar()) != '\n' && c != EOF)
                 ;
@@ -38,26 +42,33 @@ int main()
         switch (opcao)
         {
         case 1:
-            iniciarCompra();
+            iniciarCompra();      // produto.c
             break;
+
         case 2:
-            mostrarCarrinho();
+            mostrarCarrinho();    // carrinho.c
             break;
+
         case 3:
-            finalizarPedido();
+            calcularFrete();      // frete.c
+            break;
+
+        case 4:
+            gerarRelatorio();     // relatorio.c
+            break;
+
+        case 5:
+            finalizarPedido();    // relatorio.c ou outro arquivo
             printf("\nObrigado por comprar no FlavioZON!\n");
             break;
+
         default:
             printf("Opcao invalida! Tente novamente.\n");
         }
+    
     }
 
     return 0;
-}
-
-void NewFunction()
-{
-    NewFunction();
 }
 
 void NewFunction()
